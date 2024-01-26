bf6dfff0a45898aef7a0fdd198e5d4424d66b091
Lasse <lasse.l535@gmail.com>
2024-01-26 08:31:32 +0100

    movement bettr

--- expGame.py
+++ expGame.py
@@ -1,36 +1,64 @@
 import pygame
+import math
 
+#Setter opp det essensielle
 pygame.init()
 screen = pygame.display.set_mode((800, 500)) # Setter skjermen til 500x500 piksler.
 clock = pygame.time.Clock()
 running = True
-#yes
+
+#Klasse for spillerobjekt
 class Spiller:
     def __init__(self):
         self.max_hp = 3
         self.hp = self.max_hp
         self.x = screen.get_width() / 2
         self.y = screen.get_height() / 2
+        self.retning = [0, 0]
         self.size = 25
         self.color = 'darkolivegreen3'
-        self.movespeed = 5
+        self.speed = 6
         
+    #funksjon for å tegne seg selv
     def draw(self):
         self.rect = pygame.Rect(self.x - self.size / 2, self.y - self.size / 2, self.size, self.size)
         pygame.draw.rect(screen, self.color, self.rect)
 
 
-
+#Lager objekter
 spiller = Spiller()
 
+#Kjører spillet
 while running:
     # Avslutter løkken
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    #Får ett input
+    keys = pygame.key.get_pressed()
+
+    spiller.retning = [0, 0]
+
+    if(keys[pygame.K_d]) and((spiller.x) < screen.get_width()):
+        spiller.retning[0] = 1
+    if(keys[pygame.K_a]) and ((spiller.x) > 0): 
+        spiller.retning[0] = -1
+    if(keys[pygame.K_s]) and((spiller.y) < screen.get_height()):
+        spiller.retning[1] = 1
+    if(keys[pygame.K_w]) and ((spiller.y) > 0): 
+        spiller.retning[1] = -1
+
+
+    if(spiller.retning[0] != 0) and (spiller.retning[1] != 0):
+        spiller.x += spiller.retning[0] * spiller.speed / math.sqrt(2)
+        spiller.y += spiller.retning[1] * spiller.speed / math.sqrt(2)
+    else:
+        spiller.x += spiller.retning[0] * spiller.speed
+        spiller.y += spiller.retning[1] * spiller.speed
+    
 
     # Fyller skjermen med hvit farge
-    screen.fill("white")
+    screen.fill("hotpink4")
 
     spiller.draw()
 
